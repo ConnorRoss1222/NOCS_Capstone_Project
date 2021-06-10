9b076ec35f4b55f52c7120cd510271fc8e464310
Olivia Simeone <n10409491@qut.edu.au>
2021-06-10 21:10:36 +1000

    Added More Stuff
    Pause menu and some refinements

---
+++ CapstoneGame/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUi;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //If the escape key is pressed;
+        if ( Input.GetKeyDown( KeyCode.Escape ) )
+        {
+            //then resume
+            if ( GameIsPaused )
+            {
+                Resume();
+            }
+            //then pause
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    //Resuming the Game Scene
+    void Resume()
+    {
+        pauseMenuUi.SetActive( false );
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    //Pausing the game Scene
+    void Pause()
+    {
+        pauseMenuUi.SetActive( true );
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    //going back to the Menu screen from the pause menu
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene( "Main Menu" );
+    }
+
+    //Making the Menu button Quit the Game
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuUi.SetActive( false );
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+}
