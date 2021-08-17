7181c876b350dae3fa5b4938a6a95758062cea73
ConnorRoss1222 <62549182+ConnorRoss1222@users.noreply.github.com>
2021-08-17 14:49:15 +1000

    Updated NPC Interaction

--- CapstoneGame/Assets/Scripts/NPCTest.cs
+++ CapstoneGame/Assets/Scripts/NPCTest.cs
@@ -9,6 +9,9 @@ public class NPCTest : MonoBehaviour
     public GameObject ActionDisplay;
     public GameObject ActionText;
     public GameObject subText;
+    public GameObject subBox;
+    public GameObject optButton01;
+    public GameObject optButton02;
 
     void Update()
     {
@@ -33,11 +36,12 @@ public class NPCTest : MonoBehaviour
         {
             if (TheDistance <= 3)
             {
-                subText.GetComponent<Text>().text = "Hello this is a test";
+                subBox.SetActive(true);
+                subText.GetComponent<Text>().text = "Whoa! What are you doing out here little dude, the fire may have passed but this smoke still isn’t good for you. Lucky I decided to stay around for civilians or you’d be in serious trouble.";
                 this.GetComponent<BoxCollider>().enabled = false;
                 ActionDisplay.SetActive(false);
                 ActionText.SetActive(false);
-                StartCoroutine(ResetChat());
+                StartCoroutine(StartSelectConvo());
             }
         }
     }
@@ -48,10 +52,38 @@ public class NPCTest : MonoBehaviour
         ActionText.SetActive(false);
     }
 
-    IEnumerator ResetChat()
+    IEnumerator StartSelectConvo()
     {
         yield return new WaitForSeconds(2.5f);
+        subText.GetComponent<Text>().text = "Why don’t you head into town, the mayor is currently learning about bushfires himself in the hopes of preventing another and helping him research could do you both a favour.";
+        optButton01.GetComponentInChildren<Text>().text = "Yes";
+        optButton02.GetComponentInChildren<Text>().text = "No";
+        optButton01.SetActive(true);
+        optButton02.SetActive(true);
+    }
+
+    public void Option01()
+    {
+        StartCoroutine(StartYes());
+    }
+
+    IEnumerator StartYes()
+    {
+        optButton01.SetActive(false);
+        optButton02.SetActive(false);
+        subText.GetComponent<Text>().text = "Great! Here if you pass me that map you got there, I’ll mark down his location.";
+        yield return new WaitForSeconds(2.5f);
+        subBox.SetActive(false);
         subText.GetComponent<Text>().text = "";
-        this.GetComponent<BoxCollider>().enabled = false;
+        this.GetComponent<BoxCollider>().enabled = true;
+    }
+
+    public void Option02()
+    {
+        optButton01.SetActive(false);
+        optButton02.SetActive(false);
+        subBox.SetActive(false);
+        subText.GetComponent<Text>().text = "";
+        this.GetComponent<BoxCollider>().enabled = true;
     }
 }
